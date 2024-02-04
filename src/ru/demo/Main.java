91331eab0b4d0cb0c83908adaa0ce1947e3d55a0
Парфенов Евгений <esparfenov@vtb.ru>
2024-02-04 22:25:14 +0300

    Рефакторинг

--- src/ru/demo/Main.java
+++ src/ru/demo/Main.java
@@ -12,15 +12,15 @@ public class Main {
 
         var ints = List.of(5, 2, 10, 9, 4, 3, 10, 1, 13);
 
-        System.out.println("1. Список без дубликатов: " + ints.stream()
+        System.out.printf("1. Список без дубликатов: %s%n", ints.stream()
                 .distinct()
                 .toList());
-        System.out.println("2. Третье наибольшее: " + ints.stream()
+        System.out.printf("2. Третье наибольшее: %s%n", ints.stream()
                 .sorted(Comparator.reverseOrder())
                 .skip(2)
                 .findFirst()
                 .orElse(null));
-        System.out.println("3. Третье наибольшее уникальное: " + ints.stream()
+        System.out.printf("3. Третье наибольшее уникальное: %s%n", ints.stream()
                 .sorted(Comparator.reverseOrder())
                 .distinct()
                 .skip(2)
@@ -37,13 +37,13 @@ public class Main {
                 new Employee("Алексей", 48, "Директор")
         );
 
-        System.out.println("4. 3 инженера: " + empl.stream()
+        System.out.printf("4. 3 инженера: %s%n", empl.stream()
                 .filter(e -> ENGINEER.equals(e.getPosition()))
                 .sorted((o1, o2) -> o2.getAge() - o1.getAge())
                 .limit(3)
                 .map(Employee::getName)
                 .toList());
-        System.out.println("5. Ср. возраст инженеров: " + empl.stream()
+        System.out.printf("5. Ср. возраст инженеров: %s%n", empl.stream()
                 .filter(e -> ENGINEER.equals(e.getPosition()))
                 .map(Employee::getAge)
                 .mapToInt(Integer::intValue)
@@ -52,7 +52,7 @@ public class Main {
 
         var str1 = List.of("Сотрудник", "Инженер", "Технолог", "Директор", "Имя", "Возраст", "Должность");
 
-        System.out.println("6. Слово с наибольшнй длиной: " + str1.stream()
+        System.out.printf("6. Слово с наибольшей длиной: %s%n", str1.stream()
                 .max(Comparator.comparingInt(String::length))
                 .orElse(null));
 
@@ -61,7 +61,7 @@ public class Main {
         System.out.println("7. Слово/количество: " + Arrays.stream(str2.split(" "))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
 
-        System.out.println("8. Сортировка по длине и алфавиту: " + str1.stream()
+        System.out.printf("8. Сортировка по длине и алфавиту: %s%n", str1.stream()
                 .sorted((o1, o2) -> {
                             if (o1.length() == o2.length()) {
                                 return o1.compareTo(o2);
@@ -74,7 +74,7 @@ public class Main {
                 "белый синий красный зеленый сиреневый",
                 "сто двести триста четыреста пятьсот");
 
-        System.out.println("9. Самое длинное слово: " + str3.stream()
+        System.out.printf("9. Самое длинное слово: %s%n", str3.stream()
                 .flatMap(s -> Arrays.stream(s.split(" ")))
                 .min((o1, o2) -> o2.length() - o1.length())
                 .orElse(null));
